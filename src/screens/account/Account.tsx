17b2bd68cb6160f947acab384f6b6690c8fcaade
kokikono <kk.k.0421@outlook.jp>
2019-01-20 22:04:36 +0900

    create screenl

---
+++ src/screens/account/Account.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {
+    Container
+,   Header
+,   Left
+,   Right
+,   Body
+,   Title
+} from 'native-base';
+
+interface Props {}
+export default class Home extends React.Component<Props> {
+    render() {
+        return (
+            <Container>
+                <Header>
+                    <Left />
+                    <Body>
+                        <Title>アカウント</Title>
+                    </Body>
+                    <Right />
+                </Header>
+            </Container>
+        )
+    }
+}
---
+++ src/screens/index.js
@@ -0,0 +1,3 @@
+export { default as HomeScreen } from './home/Home';
+export { default as AccountScreen } from './account/Account';
+export { default as SettingScreen } from './setting/Setting';
