06524667afbd74b35e716d14cddb7aeff1d52263
kokikono <kk.k.0421@outlook.jp>
2019-01-22 20:54:56 +0900

    redux対応

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,29 +1,59 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { StateModels } from '../../redux/reducers';
+import {ToDo as ToDoType} from '../../redux/store';
+import {
+    ActionInterface, addToDo
+} from '../../redux/actions';
 
 import {
     Container
-,   Body
 ,   Content
-,   Card
-,   CardItem
-,   Text
 } from 'native-base';
+import ToDoList from '../../components/ToDoList';
+import { Navigation } from 'react-native-navigation';
 
-interface Props {}
-export default class Home extends React.Component<Props> {
+interface Props {
+    todos: Array<ToDoType>
+,   addToDo: ActionInterface
+}
+class Home extends React.Component<Props> {
+
+    componentDidMount() {
+        this.navigationEventListener = Navigation.events().bindComponent(this);
+    }
+
+    navigationButtonPressed({ buttonId }) {
+        switch(buttonId) {
+            case 'add_todo': {
+                this.props.addToDo('add title', 'add body');
+            }
+        }
+    }
+
     render() {
         return (
             <Container>
                 <Content>
-                    <Card>
-                        <CardItem>
-                            <Body>
-                                <Text>ホーム</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
+                    <ToDoList
+                        todos={this.props.todos}
+                    />
                 </Content>
             </Container>
         )
     }
 }
+
+const mapStateToProps = (state: StateModels) => ({
+    todos: state.todos
+});
+
+const mapDispatchToProps = (dispach: Dispatch) => ({
+    addToDo: (title: string, body: string) => dispach(addToDo(title, body))
+});
+
+export default connect(
+    mapStateToProps
+,   mapDispatchToProps
+)(Home);
